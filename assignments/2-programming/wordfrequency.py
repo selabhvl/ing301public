bda7701d44a3d9383048157df9ba069f55cef2ea
Victor Johansen <victoremilj@gmail.com>
2023-01-16 12:31:44 +0100

    oppgave 2

--- assignments/2-programming/wordfrequency.py
+++ assignments/2-programming/wordfrequency.py
@@ -7,86 +7,65 @@ from pathlib import Path
 # noen enkelte funskjoner som trengs for det hele til å virke.
 # Enhver funksjon kommer med en dokumentasjon som forklarer hva skal gjøres.
 
-
 def read_file(file_name):
-    """
-    Denne funksjonen får et filnavn som argument og skal gi
-    tilbake en liste av tekststrenger som representerer linjene i filen.
-    """
-    # Tips: kanksje "open"-funksjonen kunne være nyttig her: https://docs.python.org/3/library/functions.html#open
-    return NotImplemented  # TODO: Du må erstatte denne linjen
+    file = open(file_name, encoding="utf8")
+    return file.readlines()
 
-
+test = []
 def lines_to_words(lines):
-    """
-    Denne funksjonen får en liste med strenger som input (dvs. linjene av tekstfilen som har nettopp blitt lest inn)
-    og deler linjene opp i enkelte ord. Enhver linje blir delt opp der det er blanktegn (= whitespaces).
-    Desto videre er vi bare interessert i faktiske ord, dvs. alle punktum (.), kolon (:), semikolon (;),
-    kommaer (,), spørsmåls- (?) og utråbstegn (!) skal fjernes underveis.
-    Til sist skal alle ord i den resulterende listen være skrevet i små bokstav slik at "Odin" og "odin"
-    blir behandlet likt.
-    OBS! Pass også på at du ikke legge til tomme ord (dvs. "" eller '' skal ikke være med) i resultatlisten!
-
-    F. eks: Inn: ["Det er", "bare", "noen få ord"], Ut: ["Det", "er", "bare", "noen", "få", "ord"]
-    """
-    # Tips: se på "split()"-funksjonen https://docs.python.org/3/library/stdtypes.html#str.split
-    # i tillegg kan "strip()": https://docs.python.org/3/library/stdtypes.html#str.strip
-    # og "lower()": https://docs.python.org/3/library/stdtypes.html#str.lower være nyttig
-    return NotImplemented  # TODO: Du må erstatte denne linjen
-
+    words = []
+    for line in lines:
+        for word in line.split():
+            if word.isalpha():
+                words.append(word.lower())
+            else:
+                characters = filter(str.isalpha, word)
+                word = ''.join(characters)
+                if not (word == ''):
+                    words.append(word.lower())
+    return words
 
 def compute_frequency(words):
-    """
-    Denne funksjonen tar inn en liste med ord og så lager den en frekvenstabell ut av den. En frekvenstabell
-    teller hvor ofte hvert ord dykket opp i den opprinnelige innputt listen. Frekvenstabllen
-    blir realisert gjennom Python dictionaires: https://docs.python.org/3/library/stdtypes.html#mapping-types-dict
-
-    F. eks. Inn ["hun", "hen", "han", "hen"], Ut: {"hen": 2, "hun": 1, "han": 1}
-    """
-    return NotImplemented  # TODO: Du må erstatte denne linjen
-
+    wordFrequencies = {}
+    for word in words:
+        if (word in wordFrequencies):
+            wordFrequencies[word] += 1
+        else:
+            wordFrequencies[word] = 1
+    return wordFrequencies
 
 FILL_WORDS = ['og', 'dei', 'i', 'eg', 'som', 'det', 'han', 'til', 'skal', 'på', 'for', 'då', 'ikkje', 'var', 'vera']
 
-
 def remove_filler_words(frequency_table):
-    """
-    Ofte inneholder tekst koblingsord som "og", "eller", "jeg", "da". Disse er ikke så spennende når man vil
-    analysere innholdet til en tekst. Derfor vil vi gjerne fjerne dem fra vår frekvenstabell.
-    Vi har gitt deg en liste med slike koblingsord i variablen FILL_WORDS ovenfor.
-    Målet med denne funksjonen er at den skal få en frekvenstabll som input og så fjerne alle fyll-ord
-    som finnes i FILL_WORDS.
-    """
-    return NotImplemented  # TODO: Du må erstatte denne linjen
-
+    for removeKey in FILL_WORDS:
+        if removeKey in frequency_table:
+            del frequency_table[removeKey]
+    return frequency_table
 
 def largest_pair(par_1, par_2):
-    """
-    Denne funksjonen får som input to tupler/par (https://docs.python.org/3/library/stdtypes.html#tuple) der den
-    første komponenten er en string (et ord) og den andre komponenten er en integer (heltall).
-    Denne funksjonen skal sammenligne heltalls-komponenten i begge par og så gi tilbake det paret der
-    tallet er størst.
-    """
-    # OBS: Tenk også på situasjonen når to tall er lik! Vurder hvordan du vil handtere denne situasjonen
-    # kanskje du vil skrive noen flere test metoder ?!
-    return NotImplemented  # TODO: Du må erstatte denne linjen
-
+    if (par_1 < par_2):
+        return par_2
+    else:
+        return par_1
+    # gjør det enkelt og returnerer par_1 hvis den er større eller lik par_2
 
 def find_most_frequent(frequency_table):
-    """
-    Nå er det på tide å sette sammen alle bitene du har laget.
-    Den funksjonen får frekvenstabllen som innputt og finner det ordet som dykket opp flest.
-    """
-    # Tips: se på "dict.items()" funksjonen (https://docs.python.org/3/library/stdtypes.html#dict.items)
-    # og kanskje du kan gjenbruke den "largest_pair" metoden som du nettopp har laget
-    return NotImplemented  # TODO: Du må erstatte denne linjen
+    most_frequent = []
+    max_frequency = 0
+    for word in frequency_table:
+        if frequency_table[word] > max_frequency:
+            max_frequency = frequency_table[word]
+            most_frequent = [word]
+        elif frequency_table[word] == max_frequency:
+            most_frequent.append(word)
+    return ' '.join(most_frequent)
+    # i tilfelle flere ord er like frekvente returneres alle
 
 ############################################################
 #                                                          #
 # Her slutter dendelen av filen som er relevant for deg ;-)#
 #                                                          #
 ############################################################
-
 
 def main():
     file = str(Path(__file__).parent.absolute()) + "/voluspaa.txt"
@@ -95,8 +74,8 @@ def main():
     table = compute_frequency(words)
     table = remove_filler_words(table)
     most_frequent = find_most_frequent(table)
+    print(table)
     print(f"The most frequent word in {file} is '{most_frequent}'")
-
 
 if __name__ == '__main__':
     main()
