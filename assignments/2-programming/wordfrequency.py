a4676811ee4706e3d982e8281549fc27f4d932c4
mrtor <585020@stud.hvl.no>
2023-01-29 22:31:01 +0100

    oppg2

--- assignments/2-programming/wordfrequency.py
+++ assignments/2-programming/wordfrequency.py
@@ -10,13 +10,21 @@ from pathlib import Path
 # Enhver funksjon kommer med en dokumentasjon som forklarer hva skal gjøres.
 
 
+
 def read_file(file_name):
     """
     Denne funksjonen får et filnavn som argument og skal gi
     tilbake en liste av tekststrenger som representerer linjene i filen.
-    """
+
     # Tips: kanksje "open"-funksjonen kunne være nyttig her: https://docs.python.org/3/library/functions.html#open
-    return NotImplemented  # TODO: Du må erstatte denne linjen
+    return NotImplemented
+    """
+
+    "Tok meg litt tid å feilsøke dette, men måtte bestemme enkoding for å få lest filen i hele tatt"
+    "Problemet oppsto kun med voluspaa.txt, ikke small.txt"
+    import codecs
+    listfile = codecs.open(file_name, encoding='utf-8').readlines()
+    return listfile
 
 
 def lines_to_words(lines):
@@ -30,11 +38,23 @@ def lines_to_words(lines):
     OBS! Pass også på at du ikke legge til tomme ord (dvs. "" eller '' skal ikke være med) i resultatlisten!
 
     F. eks: Inn: ["Det er", "bare", "noen få ord"], Ut: ["Det", "er", "bare", "noen", "få", "ord"]
-    """
+
     # Tips: se på "split()"-funksjonen https://docs.python.org/3/library/stdtypes.html#str.split
     # i tillegg kan "strip()": https://docs.python.org/3/library/stdtypes.html#str.strip
     # og "lower()": https://docs.python.org/3/library/stdtypes.html#str.lower være nyttig
-    return NotImplemented  # TODO: Du må erstatte denne linjen
+    return NotImplemented
+    """
+
+    "Legger alle linjene sammen til ett objekt, og så splitter alle ordene opp."
+    newlines = ''.join(lines).split()
+    newlines = [item.lower() for item in newlines]
+    newlines = [item.strip() for item in newlines]
+    "Antar det er bedre måter å gjøre dette på, men med bare 4 tegn fjernet, funker det."
+    newlines = [item.strip('!') for item in newlines]
+    newlines = [item.strip(',') for item in newlines]
+    newlines = [item.strip('.') for item in newlines]
+    newlines = [item.strip(':') for item in newlines]
+    return newlines
 
 
 def compute_frequency(words):
@@ -44,8 +64,15 @@ def compute_frequency(words):
     blir realisert gjennom Python dictionaires: https://docs.python.org/3/library/stdtypes.html#mapping-types-dict
 
     F. eks. Inn ["hun", "hen", "han", "hen"], Ut: {"hen": 2, "hun": 1, "han": 1}
+
+    return NotImplemented
     """
-    return NotImplemented  # TODO: Du må erstatte denne linjen
+
+    "Bruker counter til å telle alle ordene, dette gir oss ein tabell av alle ordene med frekenvstall."
+    count = {}
+    from collections import Counter
+    count = Counter(words)
+    return count
 
 
 FILL_WORDS = ['og', 'dei', 'i', 'eg', 'som', 'det', 'han', 'til', 'skal', 'på', 'for', 'då', 'ikkje', 'var', 'vera']
@@ -58,8 +85,22 @@ def remove_filler_words(frequency_table):
     Vi har gitt deg en liste med slike koblingsord i variablen FILL_WORDS ovenfor.
     Målet med denne funksjonen er at den skal få en frekvenstabll som input og så fjerne alle fyll-ord
     som finnes i FILL_WORDS.
+
+    return NotImplemented
     """
-    return NotImplemented  # TODO: Du må erstatte denne linjen
+    newfreq = frequency_table
+    for x in FILL_WORDS:
+        if x in frequency_table:
+            del frequency_table[x]
+
+
+    "Siden det er her listen blir sist endret, bruker jeg den for å teste."
+    "Counter-objectet konverteres til ei liste av tupler, som så kan passeres inn i largest_pair"
+    "Den vil da si hvilken som er størst eller om de er like."
+    listfreq = list(newfreq.items())
+    largest_pair(listfreq[0], listfreq[1])
+
+    return newfreq
 
 
 def largest_pair(par_1, par_2):
@@ -68,20 +109,33 @@ def largest_pair(par_1, par_2):
     første komponenten er en string (et ord) og den andre komponenten er en integer (heltall).
     Denne funksjonen skal sammenligne heltalls-komponenten i begge par og så gi tilbake det paret der
     tallet er størst.
-    """
+
     # OBS: Tenk også på situasjonen når to tall er lik! Vurder hvordan du vil handtere denne situasjonen
     # kanskje du vil skrive noen flere test metoder ?!
-    return NotImplemented  # TODO: Du må erstatte denne linjen
+    """
+    #return NotImplemented
+
+    if par_1 > par_2:
+        print("par_1 is bigger!")
+    elif par_1 == par_2:
+        print("They are equal!")
+    elif par_1 < par_2:
+        print("par_2 is bigger!")
 
 
 def find_most_frequent(frequency_table):
     """
     Nå er det på tide å sette sammen alle bitene du har laget.
     Den funksjonen får frekvenstabllen som innputt og finner det ordet som dykket opp flest.
-    """
+
     # Tips: se på "dict.items()" funksjonen (https://docs.python.org/3/library/stdtypes.html#dict.items)
     # og kanskje du kan gjenbruke den "largest_pair" metoden som du nettopp har laget
-    return NotImplemented  # TODO: Du må erstatte denne linjen
+    """
+    #return NotImplemented
+
+    "Bruker most_common funksjonen til Counter"
+    most_frequent = frequency_table.most_common(1)[0][0]
+    return  most_frequent
 
 
 ############################################################
@@ -92,6 +146,7 @@ def find_most_frequent(frequency_table):
 
 
 def main():
+
     file = str(Path(__file__).parent.absolute()) + "/voluspaa.txt"
     lines = read_file(file)
     words = lines_to_words(lines)
