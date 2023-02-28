2d997b0024778947dffb8bbeb9ccf43749779588
Patrick Stünkel <webminz@googlemail.com>
2023-02-28 16:27:14 +0100

    Update lecture-sql.sql

--- examples/09_databases_design/lecture-sql.sql
+++ examples/09_databases_design/lecture-sql.sql
@@ -1,5 +1,9 @@
 SELECT * FROM measurements m ;
 
+SELECT * FROM sensors s ;
+
+SELECT * FROM routes r ;
+
 SELECT m.sensor, max(value), min(value), avg(value) FROM routes r -- kildetabell
 INNER JOIN measurements m  -- kobler sammen med rutemålinger
 ON DATETIME(r.start_ts) <= DATETIME(m.ts) -- der de ligger i intervall
@@ -25,3 +29,37 @@ SELECT * FROM sensors s ;
 
 --Legg til en kolonne
 ALTER TABLE sensors ADD COLUMN name TEXT;
+
+-- Slett en kolonne
+ALTER TABLE sensors DROP COLUMN name;
+
+
+
+CREATE TABLE sensors_new (
+	id INT NOT NULL,
+	sensor_type TEXT NULL,
+	PRIMARY KEY (id) -- nytt: primærnøkkel
+);
+
+INSERT INTO sensors_new VALUES (1, 'distance');
+INSERT INTO sensors_new VALUES (1, 'speed'); -- dette går ikke fordi den strider mot PK constraint
+
+CREATE TABLE measurements_new(
+	sensor_id INT NOT NULL,
+	ts TEXT NOT NULL,
+	value REAL NOT NULL,
+	PRIMARY KEY (sensor_id, ts),
+	FOREIGN KEY (sensor_id) REFERENCES sensors_new (id)
+);
+
+INSERT INTO measurements_new VALUES (2, '2021-01-01', 42);
+
+SELECT * FROM measurements_new ;
+
+
+
+
+
+
+
+
