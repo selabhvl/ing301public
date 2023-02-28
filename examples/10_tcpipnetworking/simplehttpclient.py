606526b780e1e1714daab2688c52c1eef73a230c
Lars Michael Kristensen <lmkr@users.noreply.github.com>
2023-02-28 16:33:23 +0100

    lagt til simpel http klient

---
+++ examples/10_tcpipnetworking/simplehttpclient.py
@@ -0,0 +1,16 @@
+# NOTE: request module must be installed using:
+# python3 -m pip install requests
+
+import requests
+
+# Let the user provide the URL of the resource to be retreived
+url = input("URL:>")
+
+# Send the HTTTP Get Request
+response = requests.get(url)
+
+# Print the body of the HTTP response being returned
+print(response.text)
+
+response.close()
+
