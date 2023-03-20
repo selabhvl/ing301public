70419b045b04fd6655f695d9cc05b0b9235486cd
Lars Michael Kristensen <lmkr@hvl.no>
2023-03-20 14:43:39 +0100

    revised main

--- examples/12_restapi_webservices/main.py
+++ examples/12_restapi_webservices/main.py
@@ -3,6 +3,7 @@
 # https://fastapi.tiangolo.com/tutorial/
 
 # Running the REST services in the uvicorn web server
+# uvicorn main:app --reload
 
 from fastapi import FastAPI, Response, status
 
@@ -13,17 +14,17 @@ routes = Routes(next_rid=1, routes=list())
 
 
 @app.get("/")
-async def root():
+def root():
     return {"message": "Welcome to the bike computer REST API - Powered by FastAPI"}
 
 
 @app.get("/route/")
-async def read_routes():
+def read_routes():
     return routes.routes
 
 
 @app.get("/route/{rid}")
-async def read_route(rid: int, response: Response):
+def read_route(rid: int, response: Response):
     route = routes.read_route(rid)
     if route:
         return route
@@ -34,7 +35,7 @@ async def read_route(rid: int, response: Response):
 
 
 @app.put("/route/{rid}")
-async def update_route(rid: int, route: Route, response: Response):
+def update_route(rid: int, route: Route, response: Response):
     updated_route = routes.update_route(rid, route)
     if updated_route:
         return updated_route
@@ -45,13 +46,13 @@ async def update_route(rid: int, route: Route, response: Response):
 
 
 @app.post("/route/", status_code=201)
-async def create_route(route: Route):
+def create_route(route: Route):
     added_route = routes.create_route(route)
     return added_route
 
 
 @app.delete("/route/{rid}")
-async def delete_route(rid: int, response: Response):
+def delete_route(rid: int, response: Response):
     route = routes.delete_route(rid)
     if route:
         return route
@@ -63,7 +64,7 @@ async def delete_route(rid: int, response: Response):
 
 
 @app.get("/route/{rid}/gpspoint")
-async def read_gps_points(rid: int, response: Response):
+def read_gps_points(rid: int, response: Response):
     gps_points = routes.read_gpspoints(rid)
     if gps_points:
         return gps_points
@@ -74,7 +75,7 @@ async def read_gps_points(rid: int, response: Response):
 
 
 @app.get("/route/{rid}/gpspoint/{pid}")
-async def read_gps_point(rid: int, pid: int, response: Response):
+def read_gps_point(rid: int, pid: int, response: Response):
     print(f"rid: {rid} pid: {pid}")
     gps_point = routes.read_gpspoint(rid, pid)
     if gps_point:
@@ -86,7 +87,7 @@ async def read_gps_point(rid: int, pid: int, response: Response):
 
 
 @app.put("/route/{rid}/gpspoint/{pid}")
-async def update_point(rid: int, pid: int, gps_point: GPSPoint, response: Response):
+def update_point(rid: int, pid: int, gps_point: GPSPoint, response: Response):
     gps_point = routes.update_gps_point(rid, pid, gps_point)
     if gps_point:
         return gps_point
@@ -97,13 +98,13 @@ async def update_point(rid: int, pid: int, gps_point: GPSPoint, response: Respon
 
 
 @app.post("/route/{rid}/gpspoint", status_code=201)
-async def create_gps_point(rid: int, gps_point: GPSPoint):
+def create_gps_point(rid: int, gps_point: GPSPoint):
     added_gps_point = routes.create_gpspoint(rid, gps_point)
     return added_gps_point
 
 
 @app.delete("/route/{rid}/gpspoint/{pid}")
-async def delete_gps_point(rid: int, pid: int, response: Response):
+def delete_gps_point(rid: int, pid: int, response: Response):
     gps_point = routes.delete_gps_point(rid, pid)
     if gps_point:
         return gps_point
