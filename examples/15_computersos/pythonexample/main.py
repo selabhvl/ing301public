3f5f27d1256266b4b9379e70fa7772ffa7d26fe4
Lars Michael Kristensen <lmkr@users.noreply.github.com>
2023-04-07 09:28:00 +0200

    python byte code example

---
+++ examples/15_computersos/pythonexample/main.py
@@ -0,0 +1,10 @@
+# https://www.geeksforgeeks.org/understanding-the-execution-of-python-program/
+
+a = 10
+b = 10
+print("Sum ", (a+b))
+
+# python3 -m py_compile main.py
+# python3 main.cpython-311.pyc
+# python3 -m dis main.py
+# hexdump main.cpython-311.pyc 
