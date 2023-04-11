ad9e753cd5dd582aedd1d1278242469257ea950e
Lars Michael Kristensen <lmkr@hvl.no>
2023-04-11 11:43:22 +0200

    updated plan

--- examples/15_computersos/pythonexample/main.py
+++ examples/15_computersos/pythonexample/main.py
@@ -1,11 +1,9 @@
-# https://www.geeksforgeeks.org/understanding-the-execution-of-python-program/
+filename = input("Filename: ")
 
-a = 10
-b = 10
-print("Sum ", (a+b))
+line_no = 1
 
-# python3 main.py
-# python3 -m py_compile main.py
-# python3 main.cpython-311.pyc
-# python3 -m dis main.py
-# hexdump main.cpython-311.pyc
+with open(filename, 'r') as file:
+
+    for line in file:
+        print(f'{line_no:} {line}', end='')
+        line_no += 1
