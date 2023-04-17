e9e6fbace08c00ad6585ecff8919330892f48e9c
Lars Michael Kristensen <lmkr@hvl.no>
2023-04-17 13:18:37 +0200

    added config info in cloud service file

--- examples/16_iot/cloudservice.py
+++ examples/16_iot/cloudservice.py
@@ -1,16 +1,23 @@
-
+# LOCAL
 # uvicorn cloudservice:app - -reload
+
+# CLOUD AZURE
+# NOTE: Private IP for running web server, External IP from the Arduino
+# EXTERNAL IP: 51.105.124.161
+# uvicorn --host 10.10.8.5 --port 8080 cloudservice:app
 
 from fastapi import FastAPI, Response, status
 from counter import Counter
 
 app = FastAPI()
 
-counters = Counter(red=0,green=0)
+counters = Counter(red=0, green=0)
+
 
 @app.get("/")
 def root():
     return {"message": "Welcome to Counter Cloud REST API - Powered by FastAPI"}
+
 
 @app.get("/counters")
 def get_counters():
