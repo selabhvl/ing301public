0e59e4f875fc356279e23f8f0cd9b61647048445
Lars Michael Kristensen <lmkr@hvl.no>
2023-04-18 10:53:38 +0200

    added azure dashboard link

--- examples/16_iot/cloudservice.py
+++ examples/16_iot/cloudservice.py
@@ -2,6 +2,7 @@
 # uvicorn cloudservice:app - -reload
 
 # CLOUD AZURE
+# https://portal.azure.com/
 # NOTE: Private IP for running web server, External IP from the Arduino
 # EXTERNAL IP: 51.105.124.161
 # uvicorn --host 10.10.8.5 --port 8080 cloudservice:app
