e1461f50774e3a626c4d4466fa20b9c69a8de963
Patrick Stünkel <webminz@googlemail.com>
2025-01-28 15:49:55 +0100

    final version gpslogs

--- weeks/05/gps_analyser_lecture.py
+++ weeks/05/gps_analyser_lecture.py
@@ -1,17 +1,21 @@
 # Python Modul importering
 import haversine
+from datetime import datetime, timedelta
 
 class Point:
     
     def __init__(self, linje: str):
         delt_opp = linje.split(",")
-        self.ts = delt_opp[0] 
+        self.ts = datetime.fromisoformat(delt_opp[0])
         self.latitude = float(delt_opp[1])
         self.longitude = float(delt_opp[2])
         self.height = float(delt_opp[3])
 
     def distance_to(self, other):
         return haversine.distance(self.longitude, self.latitude, other.longitude, other.latitude)
+
+    def time_diff(self, other):
+        return other.ts - self.ts
 
     def __sub__(self, other):
         return self.distance_to(other)
@@ -41,9 +45,11 @@ points = read_file(filnavn)
 last = points[0]
 total_distanse = 0
 max_hoyde = last
+totalt_time = timedelta(seconds=0)
 for current in points[1:]:
 
     distance = current - last
+    totalt_time += last.time_diff(current)
 
     # regne ut distanse
     sist = current
@@ -55,4 +61,5 @@ for current in points[1:]:
 
 print("Høyste punkt på turen var på " + str(max_hoyde.height) + "m")
 print("Turen var på " + str(round(total_distanse / 1000, 2)) + "km")
+print("Turen tok " + str(totalt_time) + "")
 
