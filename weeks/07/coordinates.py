d79c4af205e726cfc566563ed0edc77c94d7a38b
Patrick Stünkel <webminz@googlemail.com>
2025-02-11 08:40:25 +0100

    material for week 7

---
+++ weeks/07/coordinates.py
@@ -0,0 +1,48 @@
+import math
+
+
+# earth radius in meters
+R = 6371000
+
+def _distance(latitude1: float, longitude1: float, latitude2: float, longitude2: float) -> float:
+    """
+    Distance between two GPS points in meters as per the haversine formula.
+
+    Reading link:
+    https://en.wikipedia.org/wiki/Haversine_formula
+    """
+    phi_1 = math.radians(latitude1)
+    phi_2 = math.radians(latitude2)
+
+    delta_phi = math.radians(latitude2 - latitude1)
+    delta_delta = math.radians(longitude2 - longitude1)
+
+    a = _compute_a(phi_1, phi_2, delta_phi, delta_delta)
+
+    c = _compute_c(a)
+
+    d = R * c
+
+    return d
+
+
+def _compute_c(a):
+    return 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+
+def _compute_a(phi_1: float, phi_2: float, delta_phi: float, delta_delta: float) -> float:
+    return math.sin(delta_phi / 2) * math.sin(delta_phi / 2) + math.cos(phi_1) * math.cos(phi_2) * (
+            math.sin(delta_delta / 2) * math.sin(delta_delta / 2))
+
+
+class Coordinate:
+    
+    def __init__(self, latitude: float, longitude: float):
+        self.latitude = latitude
+        self.longitude = longitude
+
+    def distance_to(self, other):
+        return _distance(self.longitude, self.latitude, other.longitude, other.latitude)
+
+    def __sub__(self, other):
+        return self.distance_to(other)
---
+++ weeks/07/sykkelcomputer.py
@@ -0,0 +1,87 @@
+from datetime import datetime
+from coordinates import Coordinate
+import random
+
+
+
+class Sensor:
+
+    def __init__(self):
+        self.last_measurement_timestamp = None
+
+    def measure(self):
+        self.last_measurement_timestamp = datetime.now()
+
+
+class GpsSensor(Sensor):
+
+    def measure(self):
+        super().measure()
+        # random location stored as Coordinate
+        self.last_measurement = Coordinate(random.random() * 60, random.random() * 5)
+
+class Speedometer(Sensor):
+
+    def measure(self):
+        super().measure()
+        # random speed stored as float
+        self.last_measurement = random.random() * 30
+
+
+
+class TemperatureSensor(Sensor):
+
+    
+    def measure(self):
+        super().measure()
+        # random speed stored as float
+        self.last_measurement = random.random() * 10
+
+
+class HeartFrequencySensor(Sensor):
+    
+     def measure(self):
+        super().measure()
+        # random heart frequency stored as float
+        self.last_measurement = random.random() * 100
+
+
+class BikeComputer(Sensor):
+
+    def __init__(self):
+        self.sensors = []
+
+    def add_sensor(self, s: Sensor):
+        self.sensors.append(s)
+
+
+
+# main 
+c = BikeComputer()
+c.add_sensor(GpsSensor())
+c.add_sensor(Speedometer())
+c.add_sensor(HeartFrequencySensor())
+c.add_sensor(TemperatureSensor())
+
+# let all sensors measure
+for s in c.sensors:
+    s.measure()
+
+# print current measurements
+for s in c.sensors:
+    print(f"{s.last_measurement_timestamp.isoformat()}: {s.last_measurement}")
+
+
+
+
+
+
+
+class GPSPoint:
+    pass
+
+class Route:
+    pass
+
+class Display:
+    pass
