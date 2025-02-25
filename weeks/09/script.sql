049e3431421dadb59de44b3ee8d4000a4ff923c4
Patrick Stünkel <webminz@googlemail.com>
2025-02-25 15:00:26 +0100

    halfway result week 9

--- weeks/09/script.sql
+++ weeks/09/script.sql
@@ -1,5 +1,24 @@
-SELECT sensor, value 
-FROM measurements
-WHERE sensor = 2
-ORDER BY value desc
-LIMIT 3;
+SELECT sensor, value  -- velg kolonner
+FROM measurements -- velg kildetabell
+WHERE sensor = 2 -- velg rader: filtrering
+ORDER BY value desc -- velg sortering
+LIMIT 3; -- ta N første rader
+
+
+SELECT *
+FROM routes, sensors -- kryssprodukt av rader
+WHERE route_no = id;
+
+SELECT * FROM routes;
+
+SELECT * FROM sensors;
+
+
+-- spørring fra forskjellige tabeller
+SELECT *
+FROM measurements , sensors
+WHERE sensor = id
+AND sensor_type = 'speed'
+ORDER BY value;
+
+
