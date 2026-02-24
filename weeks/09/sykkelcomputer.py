b75114773b6293c1938b2196f90923167f25b483
Patrick Stünkel <webminz@googlemail.com>
2026-02-24 15:02:00 +0100

    eksempel av en iterator

--- weeks/09/sykkelcomputer.py
+++ weeks/09/sykkelcomputer.py
@@ -1,15 +1,50 @@
 class Sykkelcomputer:
 
-    def __init__(self, sensor: Sensor):
-        # 1..* mutlitplisitett
-        self.primaer_sensor = sensor
+    def __init__(self, startposisjon: Position):
+        # 1..* multiplisitett
+        self.primaer_sensor = GPSSensor(self, startposisjon)
         self.rest_sensorer = []
+
+    # Fassade
+    def legg_til_faartsmaaler(self, start_hastighet: float):
+        self.rest_sensorer.append(Fartsmaaler(self, start_hastighet))
+
+    def __iter__(self):
+        return ComputerSensorIterator(self)
+
+    # ...
+
+class ComputerSensorIterator:
+
+    def __init__(self, computer: Sykkelcomputer):
+        self.computer = computer
+        self.poisition = -1
+
+    def __next__(self):
+        if self.poisition == -1:
+            result = self.computer.primaer_sensor
+            self.poisition += 1
+            return result
+        else:
+            if self.poisition == len(self.computer.rest_sensorer):
+                raise StopIteration()
+            else:
+                result = self.computer.rest_sensorer[self.poisition]
+                self.poisition += 1
+                return result
+        
+
+
     
 
 class Sensor:
 
     def __init__(self, computer: Sykkelcomputer):
         self.computer = computer
+
+    
+    def record(self) -> str:
+        pass # abstrakt
 
 
 class Position:
@@ -18,6 +53,9 @@ class Position:
         self.lengdegrad = lengdegrad
         self.breddegrad = breddegrad
         self.hoeyde = hoeyde
+
+    def __repr__(self):
+        return "(" + str(self.lengdegrad) + ", " + str(self.breddegrad) + ")"
     
 
 class GPSSensor(Sensor):
@@ -25,6 +63,9 @@ class GPSSensor(Sensor):
     def __init__(self, computer, position: Position):
         super().__init__(computer)
         self.position = position
+
+    def record(self) -> str:
+        return str(self.position)
 
 
 
@@ -34,6 +75,9 @@ class Fartsmaaler(Sensor):
     def __init__(self, computer, fart_i_ms: float):
         super().__init__(computer)
         self.fart_i_ms = fart_i_ms
+
+    def record(self) -> str:
+        return str(self.fart_i_ms) + " m/s"
     
 
 class TemperaturSensor(Sensor):
@@ -41,6 +85,9 @@ class TemperaturSensor(Sensor):
     def __init__(self, computer, temp_i_C: float):
         super().__init__(computer)
         self.temp_i_C = temp_i_C
+
+    def record(self) -> str:
+        return str(self.temp_i_C) + " Degrees Celsius"
     
 
 class Pulsklokke(Sensor):
@@ -49,5 +96,14 @@ class Pulsklokke(Sensor):
         super().__init__(computer)
         self.hjertefrekvens_i_bpm = hjertefrekvens_i_bpm
 
-    def op(self):
-        pass
+    def record(self)  -> str:
+        return str(self.hjertefrekvens_i_bpm) + " b/m"
+    
+
+computer = Sykkelcomputer(Position(60.5, 5.5, 20))
+computer.legg_til_faartsmaaler(20.0)
+
+for sensor in computer:
+    print("Fikk måling: " +  sensor.record())
+
+
