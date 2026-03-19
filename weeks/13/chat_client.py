4987abaab684f7910e6264ef0765b3cc76a7eb2b
Lars Michael Kristensen <lmkr@users.noreply.github.com>
2026-03-19 14:44:11 +0100

    revised chat example

--- weeks/13/chat_client.py
+++ weeks/13/chat_client.py
@@ -6,13 +6,14 @@ server_address = "127.0.0.1"
 server_port = 9002
 
 client_socket.connect((server_address, server_port))
+print(f"Client connected to {server_address}")
 
 msg = ""
 while msg != "bye":
-    msg = input("client: ")
+    msg = input("Client: ")
     client_socket.sendall(msg.encode())
     data = client_socket.recv(1024)
-    print(f"server: {data.decode()}")
+    print(f"From server: {data.decode()}")
 
 print("Connection terminated.")
 
--- weeks/13/chat_server.py
+++ weeks/13/chat_server.py
@@ -4,7 +4,6 @@ server_address = "127.0.0.1"
 server_port = 9002
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-
 server.bind((server_address, server_port))
 server.listen(1)
 
@@ -12,17 +11,17 @@ print(f"Server listening on {server_address}:{server_port}")
 
 conn, client_address = server.accept()
 
-print(f"Established connection to {client_address}")
+print(f"Server accepted connection from {client_address}")
 
 msg : str = ""
 while msg !=  "bye":
-    msg = input("server: ")
-    conn.sendall(msg.encode("utf-8"))
+    print("Server waiting to receive: ")
     data = conn.recv(1024)
-    print(f"client: " + data.decode("utf-8"))
+    msg = data.decode("utf-8")
+    print(f"Server received: " + msg)
+    conn.sendall("OK".encode("utf-8"))
 
-conn.sendall("bye".encode("utf-8"))
 conn.close()
 server.close()
 
-print("server shutting down")
+print("Server shutting down")
