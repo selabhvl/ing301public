750e4f06b1656af0ba395ffa4ffa5e353c534c2a
Lars Michael Kristensen <lmkr@users.noreply.github.com>
2026-04-07 13:03:27 +0200

    added bruno teste for FastAPI hello example

--- weeks/15/restserverside/helloserver.py
+++ weeks/15/restserverside/helloserver.py
@@ -1,11 +1,16 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Message(BaseModel):
+
+    content:str
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+def read_item(item_id: int, message: Message):
+    return {"item_id": item_id, "message": message}
